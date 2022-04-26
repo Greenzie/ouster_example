2e765a343f126473cbc6881c709d4380a8c065ef
Taylor <thwentzel98@gmail.com>
2022-04-26 15:48:28 -0400

    This should replicate the 64 camera

--- ouster_ros/src/ros.cpp
+++ ouster_ros/src/ros.cpp
@@ -126,18 +126,20 @@ void scan_to_cloud(const ouster::XYZLut& xyz_lut,
     auto points = ouster::cartesian(range, xyz_lut);
 
     for (auto u = 0; u < ls.h; u++) {
-        for (auto v = 0; v < ls.w; v++) {
-            const auto xyz = points.row(u * ls.w + v);
-            const auto ts = (ls.header(v).timestamp - scan_ts).count();
-            cloud(v, u) = ouster_ros::Point{
-                {{static_cast<float>(xyz(0)), static_cast<float>(xyz(1)),
-                  static_cast<float>(xyz(2)), 1.0f}},
-                static_cast<float>(signal(u, v)),
-                static_cast<uint32_t>(ts),
-                static_cast<uint16_t>(reflectivity(u, v)),
-                static_cast<uint8_t>(u),
-                static_cast<uint16_t>(near_ir(u, v)),
-                static_cast<uint32_t>(range(u, v))};
+        if (u % 2 == 0) {
+            for (auto v = 0; v < ls.w; v++) {
+                const auto xyz = points.row(u * ls.w + v);
+                const auto ts = (ls.header(v).timestamp - scan_ts).count();
+                cloud(v, u) = ouster_ros::Point{
+                    {{static_cast<float>(xyz(0)), static_cast<float>(xyz(1)),
+                      static_cast<float>(xyz(2)), 1.0f}},
+                    static_cast<float>(signal(u, v)),
+                    static_cast<uint32_t>(ts),
+                    static_cast<uint16_t>(reflectivity(u, v)),
+                    static_cast<uint8_t>(u),
+                    static_cast<uint16_t>(near_ir(u, v)),
+                    static_cast<uint32_t>(range(u, v))};
+            }
         }
     }
 }
